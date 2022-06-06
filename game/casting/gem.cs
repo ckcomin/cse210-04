8624f904d8a6693df702f3873b134b14f0850d78
= <wildman700@gmail.com>
2022-06-06 11:18:24 -0600

    yo

--- game/casting/gem.cs
+++ game/casting/gem.cs
@@ -6,6 +6,8 @@ namespace Unit04.Game.Casting
 
         }
 
+        //we might need to get the start possition of the gem here (might already be done in a different class)
+
         //if gems posiitoin is greater than {coordinates of the bottom of the screen}
         //then have the gem go down every gameloop (tick)
 
