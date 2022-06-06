f23aea0cbecfee083262c5ddabf71dbb27fd956e
= <wildman700@gmail.com>
2022-06-06 10:53:02 -0600

    yo

---
+++ game/casting/gem.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+namespace Unit04.Game.Casting
+{
+    public class gem : Actor {
+        public rock {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
