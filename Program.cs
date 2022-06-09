148b245897b62ba58ea15bf71e6e39842bec6a38
Reed Cannon <reed.cannon97@gmail.com>
2022-06-09 17:59:10 -0600

    changing artifact creation

--- Program.cs
+++ Program.cs
@@ -53,31 +53,30 @@ namespace Unit04
             player.SetPosition(new Point(MAX_X / 2, MAX_Y / 2));
             cast.AddActor("player", player);
 
-        //     // create the artifacts
-        //     Random random = new Random(); (We need this but for gems and rocks.)
-        //     for (int i = 0; i < DEFAULT_ARTIFACTS; i++)
-        //     {
-        //         string text = ((char)random.Next(33, 126)).ToString();
-        //         string message = messages[i];
+             // create the artifacts
+             Random random = new Random(); 
+             for (int i = 0; i < DEFAULT_GEMS; i++)
+             {
+                 string text = "*";
 
-        //         int x = random.Next(1, COLS);
-        //         int y = random.Next(1, ROWS);
-        //         Point position = new Point(x, y);
-        //         position = position.Scale(CELL_SIZE);
+                 //int x = random.Next(1, COLS);
+                //int y = random.Next(1, ROWS);
+                 //Point position = new Point(x, y);
+                 //position = position.Scale(CELL_SIZE);
 
-        //         int r = random.Next(0, 256);
-        //         int g = random.Next(0, 256);
-        //         int b = random.Next(0, 256);
-        //         Color color = new Color(r, g, b);
+                 int r = random.Next(0, 256);
+                 int g = random.Next(0, 256);
+                 int b = random.Next(0, 256);
+                 Color color = new Color(r, g, b);
 
-        //         Artifact artifact = new Artifact();
-        //         artifact.SetText(text);
-        //         artifact.SetFontSize(FONT_SIZE);
-        //         artifact.SetColor(color);
-        //         artifact.SetPosition(position);
-        //         artifact.SetMessage(message);
-        //         cast.AddActor("artifacts", artifact);
-        //     }
+                 gem gem = new gem();
+                 gem.SetText(text);
+                 gem.SetFontSize(FONT_SIZE);
+                 gem.SetColor(color);
+                 //artifact.SetPosition(position);
+                 artifact.SetMessage(message);
+                 cast.AddActor("artifacts", artifact);
+             }
 
         //     // start the game
         //     KeyboardService keyboardService = new KeyboardService(CELL_SIZE);
